2907d2377793b986d6edd800db417c4bb3fcac8c
Dylan Shaffer (RIT Student) <drs7740@rit.edu>
2024-09-23 13:53:44 -0400

    Started Reflections

--- test.glsl
+++ test.glsl
@@ -24,8 +24,10 @@ bool shadowCast(vec3 point, vec3 sun){
     return false;
 }
 
-vec4 castRay(vec3 origin, vec3 rayDir, int recursionCount){
-    vec4 fragColor = vec4(rayDir.x, rayDir.y, rayDir.z, 1.0);
+vec3 castRay(vec3 origin, vec3 rayDir, int recursionCount){
+    if(recursionCount <= 0)
+        return vec3(0,0,0);
+    vec3 fragColor = vec3(rayDir.x, rayDir.y, rayDir.z);
     
     float minDist = 1000.;
     
@@ -33,11 +35,11 @@ vec4 castRay(vec3 origin, vec3 rayDir, int recursionCount){
     if((planeHeight - origin.y) / rayDir.y < minDist && rayDir.y / (planeHeight - origin.y) > 0.){
         minDist = (planeHeight - origin.y) / rayDir.y;
 
-        float grid = mod(floor((rayDir * (planeHeight - origin.y) / rayDir.y ).x + origin.x) + floor((rayDir * (planeHeight - origin.y) / rayDir.y ).z + origin.z), 2.);
-        fragColor = vec4(grid, grid, grid, 1);
+        float grid = mod(floor((rayDir * ((planeHeight - origin.y) / rayDir.y) ).x + origin.x) + floor((rayDir * ((planeHeight - origin.y) / rayDir.y) ).z + origin.z), 2.);
+        fragColor = vec3(grid, grid, grid);
         
         if(shadowCast(rayDir * (planeHeight - origin.y) / rayDir.y + origin, sun)){
-            fragColor = vec4(0, 0, 0, 1);
+            fragColor = vec3(0, 0, 0);
         }
     }
     
@@ -50,7 +52,7 @@ vec4 castRay(vec3 origin, vec3 rayDir, int recursionCount){
         float rt = b*b - 4. * a * c;
         
         if(rt > 0.){
-            float distance = (-b - sqrt(rt)) / 2. * a;
+            float distance = (-b - sqrt(rt)) / 2. * a; //transparency by taking the positive root(starting a ray on the other side of the sphere)
             vec3 point = origin + rayDir * distance;
             vec3 normal = normalize(point - spherePositions[i]);
             
@@ -58,27 +60,31 @@ vec4 castRay(vec3 origin, vec3 rayDir, int recursionCount){
             
             if(distance < minDist){
                 minDist = distance;
-                fragColor = vec4(luminosity * sphereColors[i], 1); //eventually save depth texture to seperate image so I can do shadows after calculating depth
+                fragColor = vec3(luminosity * sphereColors[i]); //eventually save depth texture to seperate image so I can do shadows after calculating depth
                 
+                if(reflectionAmount[i] > 0.){   //R = V - 2 * (N dot V) * N
+                    fragColor = fragColor * (1. - reflectionAmount[i]);// + castRay(point, rayDir - 2. * dot(normal, rayDir) * normal, recursionCount - 1) * reflectionAmount[i];
+                }
+
                 if(shadowCast(point, sun)){
-                    fragColor = vec4(0, 0, 0, 1);
+                    fragColor = vec3(0, 0, 0);
                 }
             }
         }
     }
 
-    return fragColor;
+    return vec3(fragColor);
 }
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
-    sun = vec3(0. * sin(iTime), -20, 5);
+    sun = vec3(10, -20, 5);
     vec2 uv = fragCoord;
     
     vec3 rayDir = vec3((iResolution.x/2.-uv.x)/iResolution.y, (iResolution.y/2.-uv.y)/iResolution.y, .5);
     rayDir = normalize(rayDir);
     
-    fragColor = castRay(vec3(10. * sin(iTime),0,0), rayDir, 3);
+    fragColor = vec4(castRay(vec3(10. * sin(iTime),0,0), rayDir, 3), 1);
     
     //fragColor = vec4(minDist / 40., minDist/40., minDist / 40., 1);
 }
